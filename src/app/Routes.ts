20fd4b372ee243ae0c91921d0cdcdd0868296e12
Alisson-Py <alissilva1231@hotmail.com>
2021-06-17 05:59:15 -0300

    fix: change http method and add route param

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -12,15 +12,15 @@ routes.get('/', (req, res) => (res.json({ok: 'server running'})));
 routes.get('/v1/login', User.Login);
 
 routes.get('/v1/users/show', User.Index);
-routes.get('/v1/users/details', User.Show);
+routes.get('/v1/users/details/:id', User.Show);
 routes.post('/v1/users/create', User.Store);
-routes.put('/v1/users/update', User.Update);
-routes.delete('/v1/users/delete', User.Delete);
+routes.put('/v1/users/update/:id', User.Update);
+routes.delete('/v1/users/delete/:id', User.Delete);
 
 routes.get('/v1/oldman/show', Oldman.Index);
-routes.get('/v1/oldman/details', Oldman.Show);
-routes.get('/v1/oldman/create', Oldman.Store);
-routes.get('/v1/oldman/update', Oldman.Update);
-routes.get('/v1/oldman/delete', Oldman.Delete);
+routes.get('/v1/oldman/details/:id', Oldman.Show);
+routes.post('/v1/oldman/create', Oldman.Store);
+routes.put('/v1/oldman/update/:id', Oldman.Update);
+routes.delete('/v1/oldman/delete/:id', Oldman.Delete);
 
 export default routes;
