3227bb2a5cb19c71c30181c4f51b18f472861399
Alisson-Py <alissilva1231@hotmail.com>
2021-06-17 16:46:39 -0300

    feat: implementation authentication in endpoints

--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -3,6 +3,7 @@ const routes = Router();
 
 import OldmanControllers from './controllers/OldmanController';
 import UserControllers from './controllers/UserControllers';
+import authentication from './middlewares/authentication';
 
 const Oldman = new OldmanControllers;
 const User = new UserControllers;
@@ -13,14 +14,14 @@ routes.get('/v1/login', User.Login);
 
 routes.get('/v1/users/show', User.Index);
 routes.get('/v1/users/details/:id', User.Show);
-routes.post('/v1/users/create', User.Store);
-routes.put('/v1/users/update/:id', User.Update);
-routes.delete('/v1/users/delete/:id', User.Delete);
+routes.post('/v1/users/create',authentication, User.Store);
+routes.put('/v1/users/update/:id', authentication, User.Update);
+routes.delete('/v1/users/delete/:id', authentication, User.Delete);
 
 routes.get('/v1/oldman/show', Oldman.Index);
 routes.get('/v1/oldman/details/:id', Oldman.Show);
-routes.post('/v1/oldman/create', Oldman.Store);
-routes.put('/v1/oldman/update/:id', Oldman.Update);
-routes.delete('/v1/oldman/delete/:id', Oldman.Delete);
+routes.post('/v1/oldman/create', authentication, Oldman.Store);
+routes.put('/v1/oldman/update/:id', authentication, Oldman.Update);
+routes.delete('/v1/oldman/delete/:id', authentication, Oldman.Delete);
 
 export default routes;
