03c7a40905db83c81a5de5e28ca881758bbdc986
Alisson-Py <alissilva1231@hotmail.com>
2021-06-17 16:47:55 -0300

    feat: implementetion token gen

--- src/app/controllers/UserControllers.ts
+++ src/app/controllers/UserControllers.ts
@@ -1,6 +1,8 @@
 import {Request as Req,Response as Res} from 'express';
 import User from '../models/UserModels';
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import tokenGen from '../auth/tokenGen';
 
 export default class UserControllers {
   async Login(req: Req,res: Res): Promise<Res<any>> {
@@ -19,9 +21,15 @@ export default class UserControllers {
 
       if (!await bcrypt.compare(password, user.passwordWash)) return res.status(401).json({err: 'Incorrect Password'});
 
-      return res.status(200).json({
+      return res.json({
         id: user.id,
-        userLevel: user.userLevel
+        fistName: user.firstName,
+        lastName: user.lastName,
+        userLevel: user.userLevel,
+        token: tokenGen({
+          id: user.id,
+          userLevel: user.userLevel
+        })
       });
     } catch (err) {
       console.log({log: err.message});
@@ -34,7 +42,7 @@ export default class UserControllers {
       const users  = await User.find({
         deleted: false
       });
-      if (!users) return res.json([]);
+      if (!users) return res.json(['not found']);
       return res.json(users);
     }catch(err) {
       console.log({log: err.message});
@@ -67,6 +75,7 @@ export default class UserControllers {
     } = req.body;
     try {
       await User.create({
+        id: uuid(),
         firstName,
         lastName,
         email,
@@ -93,7 +102,13 @@ export default class UserControllers {
   async Delete(req: Req,res: Res): Promise<Res<any>> {
     const id = req.params.id;
     try {
-      await User.findOneAndUpdate({
+      const user = await User.findOne({
+        id,
+        deleted: false
+      });
+      if (!user) return res.status(400).json({err: 'user not found'});
+      
+      await User.updateOne({
         id,
         deleted: false
       },{
