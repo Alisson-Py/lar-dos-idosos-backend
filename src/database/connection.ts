576071a923e984db6571c26e6634dbc202e8906e
Alisson-Py <alissilva1231@hotmail.com>
2021-06-18 23:42:15 -0300

    feat: import migrations

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import insertAdmin from './migrations/insert-admin';
 
 dotenv.config()
 const uri = process.env.DB_URI || 'no';
@@ -9,8 +10,9 @@ const connection = () => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-  }).then(() => {
-    console.log('DB Connect Successful')
+  }).then(async () => {
+    console.log('DB Connect Successful');
+    await insertAdmin();
   }).catch(err => {
     console.log({log: err.message});
   });
